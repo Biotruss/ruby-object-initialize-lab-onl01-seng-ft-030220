a1f69b1a943c0b56768cd3236812e45d6227ab08
Craig Christopher Messi <craig.messi@gmail.com>
2020-03-12 03:55:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog 
-  def initialize(dog_name, breed="Mutt")
+  def initialize(name, breed="Mutt")
     @dog_name = dog_name
   end
-  def dog_name=(dog_name)
-    @name = dog_name
+  def name=(name)
+    @name = name
   end
-  def dog_name
+  def name
     @name
   end
   def breed
